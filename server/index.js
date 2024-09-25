4d19c3322b126b7899d1d6b4ff41cdae2d705aa7
gauravthakur2121 <shishodiag12@gmail.com>
2024-09-25 20:31:55 +0530

    database fetch from the atlas and build the schema

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const express = require("express")
 const app = express();
 require("dotenv").config();
 const myroute = require('./Routes/myroutes')
+require('./Database/mydatabaseconnection')
 const myport = process.env.PORT || 8000
 
 app.use(myroute)
 app.use(express.json())
-
-
 
 app.listen(myport , ()=>{
     console.log(`server is running ${myport}`)
